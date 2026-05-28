2157ed07a6e3f3497b8cfb18063f74015aa29cf2
kaushik raj <122168016+shitcodebykaushik@users.noreply.github.com>
2026-05-29 00:59:01 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        if(nums.empty()) return false;
+        sort(nums.begin(),nums.end());
+        for(int i =0;i+1<nums.size();i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+     return false;
+    }
+    
+};
