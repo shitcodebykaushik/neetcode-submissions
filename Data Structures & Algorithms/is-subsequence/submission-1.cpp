be67245abd2b15efa2f0a28db0393a76a36ae5f4
kaushik raj <122168016+shitcodebykaushik@users.noreply.github.com>
2026-06-08 15:15:50 +0530

    Add: is-subsequence - submission-1

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        for(int j=0;j<t.size();j++){
+            if(i<s.size() && s[i] ==t[j]) {
+                i++;
+            }
+        };
+         if(i==s.size()) {
+           
+            return true;
+        }else {
+            
+            return false;
+        }
+    }
+};
